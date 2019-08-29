4c3588fcbefcb445ae336a6d0e7197649582c4c1
Ed Langley <el-github@elangley.org>
2019-08-28 23:23:14 -0500

    Add docs

--- model.lisp
+++ model.lisp
@@ -1,8 +1,25 @@
+;; [[file:~/git_repos/lisp-sandbox/todo/README.org::*model.lisp%20source%20code][model.lisp source code:1]]
+;; [[file:~/git_repos/lisp-sandbox/todo/README.org::package-include][package-include]]
 (in-package :fwoar.todo)
 
-;; todo "database" api
+;; package-include ends here
+;; [[file:~/git_repos/lisp-sandbox/todo/README.org::model-utils][model-utils]]
+(defparameter *cur-id* 0)
+(defun next-id ()
+  (incf *cur-id*))
+
+(defparameter *completed-lens*
+  (data-lens.lenses:make-hash-table-lens "completed"))
+
+(defun bool-to-yason (bool)
+  (if bool
+      'yason:true
+      'yason:false))
+;; model-utils ends here
+
 (defvar *todos* (fset:empty-map))
 
+;; [[file:~/git_repos/lisp-sandbox/todo/README.org::todolist-manipulation][todolist-manipulation]]
 (defun todos ()
   (gmap:gmap :seq
              (lambda (_ b)
@@ -10,23 +27,26 @@
                b)
              (:map *todos*)))
 
+(defun clear-todos ()
+  (setf *todos*
+        (fset:empty-map)))
+;; todolist-manipulation ends here
+
+;; [[file:~/git_repos/lisp-sandbox/todo/README.org::todo-accessor][todo-accessor]]
 (defun todo (id)
   (let ((todo (fset:@ *todos* id)))
     todo))
 
 (defun (setf todo) (new-value id)
-  (setf *todos*
-        (fset:with *todos* id new-value))
-  new-value)
+  (setf (fset:@ *todos* id)
+        new-value))
 
 (defun delete-todo (id)
   (setf *todos*
         (fset:less *todos* id)))
+;; todo-accessor ends here
 
-(defparameter *cur-id* 0)
-(defun next-id ()
-  (incf *cur-id*))
-
+;; [[file:~/git_repos/lisp-sandbox/todo/README.org::new-todo][new-todo]]
 (defun new-todo (value)
   (let ((id (next-id)))
     (setf (todo id)
@@ -36,20 +56,9 @@
             (acons "completed" 'yason:false)
             (acons "url" (format nil "http://localhost:5000/todo/~d" id)))
            :test 'equal))))
+;; new-todo ends here
 
-(defun clear-todos ()
-  (setf *todos*
-        (fset:empty-map)))
-
-
-(defparameter *completed-lens*
-  (data-lens.lenses:make-hash-table-lens "completed"))
-
-(defun bool-to-yason (bool)
-  (if bool
-      'yason:true
-      'yason:false))
-
+;; [[file:~/git_repos/lisp-sandbox/todo/README.org::update-todo][update-todo]]
 (defun update-todo (id v)
   (setf (todo id)
         (serapeum:merge-tables (or (todo id)
@@ -59,3 +68,9 @@
                                                       (alexandria:alist-hash-table
                                                        v
                                                        :test 'equal)))))
+;; update-todo ends here
+
+(defmacro with-fresh-todos (() &body body)
+  `(let ((*todos* (fset:empty-map)))
+     ,@body))
+;; model.lisp source code:1 ends here
---
+++ pprint-setup.lisp
@@ -0,0 +1,21 @@
+(in-package :cl-user)
+(defparameter *old-pprint-dispatch* *print-pprint-dispatch*)
+(setf *print-pprint-dispatch*
+      (copy-pprint-dispatch *old-pprint-dispatch*))
+
+(defun pprint-hashtable (s hash-table)
+  (pprint-logical-block (s nil)
+    (princ "#<hash-table "  s)
+    (let ((v (fset:convert 'list (fset:convert 'fset:map hash-table))))
+      (when v
+        (pprint-logical-block (s v)
+          (pprint-indent :block 0 s)
+          (loop do
+            (destructuring-bind (key . value) (pprint-pop)
+              (format s "~s: ~s" key value)
+              (pprint-exit-if-list-exhausted)
+              (princ ", " s)
+              (pprint-newline :mandatory s))))))
+    (princ #\> s)))
+
+(set-pprint-dispatch 'hash-table 'pprint-hashtable)
